57155020182cbb176e472a956a4677169c1ed5a2
azizbek <badirdinovazizbek@gmail.com>
2023-07-31 10:49:55 +0600

    last changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,44 @@ const App = () => {
     const [participants, setParticipants] = useState([])
 
     const socket = useContext(SocketServerContext)
-    useEffect(() => {
-        const getTesting = async (testingId) => {
-            const testing = await axios.get(`api/v1/testings/${testingId}`)
-            setTesting(testing.data)
-            setParticipants(testing.data.participants)
 
-            return testing.data.participants
-        }
-
-        const testingId = prompt('Enter testing id')
-
-        getTesting(testingId).then((participants) => {
-            socket.on(`addNewParticipantToRoom_${testingId}`, participant => {
-                setParticipants([...participants, participant])
-            })
-
-            socket.on(`removeParticipantToRoom_${testingId}`, participantId => {
-                setParticipants([...participants.filter(item => item.id !== participantId)])
-            })
-
-            socket.on(`updateParticipantToRoom_${testingId}`, participant => {
-                setParticipants([
-                    ...participants.filter(item => item.id !== participant.id),
-                    participant
-                ])
-            })
+    const getTesting = async (testingId) => {
+        const testing = await axios.get(`api/v1/testings/${testingId}`)
+        setTesting(testing.data)
+        setParticipants([...testing.data.participants])
+        socket.on(`addNewParticipantToRoom_${testingId}`, participant => {
+            alert('added new participant')
+            console.log(participant)
+            setParticipants([...participants, participant])
         })
 
-        return () => {
-            socket.off(`addNewParticipantToRoom_${testingId}`)
-        }
+        socket.on(`nextQuestion_${testingId}`, ({question}) => {
+            alert(`You have been moved to the question ${question}`)
+        })
+
+        socket.on(`removeParticipantToRoom_${testingId}`, participantId => {
+            setParticipants([...participants.filter(item => item.id !== participantId)])
+        })
+
+        socket.on(`startTesting_${testingId}`, (res) => {
+            alert(`The test is started`)
+        })
+
+        socket.on(`endTesting_${testingId}`, (res) => {
+            alert(`The test is ended`)
+        })
+
+        socket.on(`updateParticipantToRoom_${testingId}`, participant => {
+            setParticipants([
+                ...participants.filter(item => item.id !== participant.id),
+                participant
+            ])
+        })
+    }
+
+    useEffect(() => {
+        const testingId = prompt('Enter testing id')
+        getTesting(testingId).then(r => {})
     }, [])
 
     return (
--- src/context/SocketServerProvider.js
+++ src/context/SocketServerProvider.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { io } from 'socket.io-client'
-export const socket = io('http://localhost:8000/')
+export const socket = io('http://127.0.0.1:8000/', { path: '/testing', extraHeaders: { Authorization: '1b00f905-8b5d-4e52-995f-83865ea074bb' }})
 export const SocketServerContext = React.createContext()
 export const SocketServerProvider = SocketServerContext.Provider
